963910122f991bcef9bef43d94832d908e2b6153
Rishit Pushpkar <rishit.02pushpkar@gmail.com>
2023-02-01 15:36:47 +0530

    files added

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ const description = document.querySelector('.description');
 const searchText = document.getElementById('input-location');
 const unitChangeBtn = document.querySelector('.temp-btn');
 const errMsg = document.querySelector('.err-msg');
+const weatherImage = document.querySelector('.w-icon');
 
 
-const listOfIconsCodes = [200,202,230,233,500,511,521,522,523,610,611,621,622,623,701,800,802,802,803,804];
+const listOfIconsCodes = [804,200,202,230,233,500,511,521,522,523,610,611,621,622,623,700,800,802,803];
 
 
 // ------------------functions--------------------//
@@ -28,41 +29,33 @@ function f2c(f){
 
 // const apiLink = `https://api.openweathermap.org/data/2.5/weather?q=bhopal&APPID=903507f17d707fecd352d38301efba77&units=imperial`
 
-const request = new XMLHttpRequest();
 searchText.addEventListener('keypress',(e)=>{
     if(e.key==="Enter"){
-        request.open('GET',`https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&APPID=903507f17d707fecd352d38301efba77&units=imperial`);
-        request.send();
-
-        request.addEventListener('load',()=>{
-            const data = JSON.parse(request.responseText);
-            if(data.cod !== "404"){  
+        const request = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&APPID=903507f17d707fecd352d38301efba77&units=imperial`)
+        .then(response=>{return response.json()})
+        .then(result=>{
+            if(result.cod !== "404"){  
                 errMsg.classList.add('hidden'); 
-                console.log(data);
-                windData.textContent = data.wind.speed + ' Km/h';
-                humidityData.textContent = data.main.humidity + '%';
-                rainData.textContent = data.clouds.all + '%';
-                temp.firstChild.data = Math.round(data.main.temp.toFixed(1));
+                console.log(result);
+                windData.textContent = result.wind.speed + ' Km/h';
+                humidityData.textContent = result.main.humidity + '%';
+                rainData.textContent = result.clouds.all + '%';
+                temp.firstChild.result = Math.round(result.main.temp.toFixed(1));
 
-                if (listOfIconsCodes.includes(data.weather[0].id)){
-                    const weatherImage = document.querySelector('.w-icon');
-                    weatherImage.src = `./practice-assets/weather-icons/${data.weather[0].id}.svg`;
+                weatherImage.setAttribute('src','');
+                description.textContent = '';
+                if (listOfIconsCodes.includes(result.weather[0].id)){
+                    console.log(listOfIconsCodes.includes(result.weather[0].id));
+                    weatherImage.setAttribute('src',`./assets/${result.weather[0].id}.svg`)
+                    // weatherImage.src = `./assets/${result.weather[0].id}.svg`;
                 }else{
-                    description.textContent = data.weather[0].description;
+                    description.textContent = result.weather[0].description;
                 }
-                
-                console.log(typeof data.weather[0].id)
             }
-            else{
-                errMsg.classList.remove('hidden');
-                console.log('Location Not Found')
-            }   
-        })
-
+        });
     }
 })
 
-console.log(temp.firstChild.data)
 unitChangeBtn.addEventListener('click',()=>{
     if(unitChangeBtn.innerText==='°C'){
         temp.firstChild.data = Math.round((Number(c2f(temp.firstChild.data))).toFixed(1));
